45937bcb7433bd83a2d224eda63a31467c13e24e
vovakpro13 <vovakpro13@gmail.com>
2021-05-27 22:32:08 +0300

    init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,26 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { UserComponent } from './components/user/user.component';
+import { UsersComponent } from './components/users/users.component';
+import {UsersService} from "./services/users.service";
+import { PostsComponent } from './components/posts/posts.component';
+import { PostComponent } from './components/post/post.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserComponent,
+    UsersComponent,
+    PostsComponent,
+    PostComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/constants/urls.ts
@@ -0,0 +1,7 @@
+import {environment} from "../../environments/environment";
+const {API} = environment;
+
+export const urls = {
+  users: `${API}/users`,
+  posts: `${API}/posts`,
+}
